f010fa44879e6700fa0bce9272ee977eb6d96fb6
Inzhu060 <inzhunurbergenova@gmail.com>
2025-01-31 21:40:33 +0500

    Lab 2

---
+++ lab2/Operators.py
@@ -0,0 +1,14 @@
+x = 5
+y = 2
+print(x % y)
+
+x = 5
+x//=3
+print(x)
+
+x = 5
+y = 3
+print(x != y)
+
+x = 5
+print(x > 3 or x < 4)
---
+++ lab2/Sets_methods.py
@@ -0,0 +1,13 @@
+x = {"apple", "banana", "cherry"}
+y = {"google", "microsoft", "apple"}
+
+z = x.difference(y)
+
+print(z)
+
+x = {"a", "b", "c"}
+y = {"f", "e", "d", "c", "b", "a"}
+
+z = x.issubset(y)
+
+print(z)
