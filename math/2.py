ae25ed5023603d0e8e72ec76ce8e50bc0fc3599d
Inzhu060 <inzhunurbergenova@gmail.com>
2025-02-15 14:33:46 +0500

    lab4

---
+++ math/2.py
@@ -0,0 +1,7 @@
+import math
+
+height = float(input("Enter the height: "))
+base_first = float(input("Enter the base1: "))
+base_second = float(input("Enter the base2: "))
+area = (base_first + base_second) * height / 2
+print(area)
---
+++ math/4.py
@@ -0,0 +1,6 @@
+import math 
+
+length_base = float(input("Enter the length of base: "))
+height = float(input("Enter the height of parallelogram: "))
+area = length_base * height
+print("area:", area)
